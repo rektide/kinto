1383c224cb3d53917a98a92645e4b087b8e2fee5
Tarek Ziade <tarek@ziade.org>
2015-11-20 16:30:16 +0100

    avoid collections name collision between buckets

--- kinto/views/changes.py
+++ kinto/views/changes.py
@@ -1,37 +1,38 @@
-from collections import defaultdict
-
 from cornice import Service
 from pyramid import httpexceptions
 from pyramid.security import NO_PERMISSION_REQUIRED
 
 
 changes = Service(name='changes',
-                  description='Clear database content',
+                  description='Returns changes',
                   path='/changes')
 
 
 @changes.get(permission=NO_PERMISSION_REQUIRED)
 def changes_get(request):
-    collections = defaultdict(int)
+    res = {}
 
     def get_all(**kw):
         return request.registry.storage.get_all(**kw)[0]
 
     for bucket in get_all(collection_id='bucket', parent_id=''):
-        b_parent_id = '/buckets/%s' % bucket['id']
+        bucket_id = bucket['id']
+        b_parent_id = '/buckets/%s' % bucket_id
 
         for collection in get_all(collection_id='collection',
                                   parent_id=b_parent_id):
-
             collection_id = collection['id']
-            c_parent_id = '%s/collections/%s' % (b_parent_id, collection_id)
+            cid = '%s/collections/%s' % (b_parent_id, collection_id)
 
             for record in get_all(collection_id='record',
-                                  parent_id=c_parent_id):
-                if record['last_modified'] > collections[collection_id]:
-                    collections[collection_id] = record['last_modified']
+                                  parent_id=cid):
+                if cid in res:
+                    res[cid]['last_modified'] = max(record['last_modified'],
+                                                    res[cid]['last_modified'])
+                else:
+                    res[cid] = {'last_modified': record['last_modified'],
+                                'bucket_id': bucket_id,
+                                'collection_id': collection_id}
 
     # XXX add a filter so we display only the latest changes
-    return {'data': [{'id': colid, 'last_modified': col}
-                     for colid, col in collections.items()
-                    ]}
+    return {'data': list(res.values())}
